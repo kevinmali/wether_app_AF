5fa85e437a4ba06ba886e61d1e31bddb8c1f49ab
r33 <kevinmali0433@gmail.com>
2023-09-01 10:52:51 +0530

    intro

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'Provider/themeprovider.dart';
 import 'Views/Screens/Splace_screen.dart';
 import 'Views/Screens/Wether_page.dart';
+import 'Views/Screens/intro_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  bool isvisited = preferences.getBool("isIntroVisited") ?? false;
   runApp(MultiProvider(
     providers: [
       ListenableProvider<themeprovider>(create: (context) => themeprovider()),
@@ -12,8 +17,10 @@ void main() {
     builder: (context, child) => MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
+      initialRoute: (isvisited) ? 'Splash' : '/',
       routes: {
-        '/': (context) => Splash(),
+        '/': (context) => intro_page(),
+        'Splash': (context) => Splash(),
         'home': (context) => home(),
       },
     ),
